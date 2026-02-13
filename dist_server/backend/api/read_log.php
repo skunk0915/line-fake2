6148772bc245025e019331fe1813310bfad7db1e
mizy <skunk0915@gmail.com>
2026-02-14 02:54:45 +0900

    Stop tracking build artifacts in dist_server

--- dist_server/backend/api/read_log.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-$logFile = __DIR__ . '/push_log.txt';
-if (file_exists($logFile)) {
-    echo file_get_contents($logFile);
-} else {
-    echo "Log file not found.";
-}
